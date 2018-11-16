4df5d6dad772ed0ade731c967b0812a275f7dab4
John Bell <jwbell001@gmail.com>
2018-11-16 19:58:13 +0200

    init

---
+++ run.py
@@ -0,0 +1,6 @@
+from saber import Saber
+
+if __name__ == '__main__':
+    saber = Saber()
+
+    saber.run()
---
+++ saber.py
@@ -0,0 +1,22 @@
+import config
+import neopixel as neo
+import time
+
+class Saber(object):
+    def __init__(self):
+        self.strand_lights = config.SIDE_1_LEDS + config.SIDE_2_LEDS + config.SIDE_3_LEDS + config.SIDE_4_LEDS
+        self.strand = neo.Adafruit.NeoPixel(self.strand_lights,
+                                            config.LED_PIN,
+                                            config.LED_FREQ,
+                                            config.LED_DMA,
+                                            config.LED_INVERT,
+                                            config.LED_BRIGHTNESS,
+                                            config.LED_CHANNEL
+                                            )
+
+    def run(self):
+        while True:
+            for i in range(self.strand_lights):
+                self.strand.setPixelColor(i, neo.Color(255, 0, 0))
+                self.strip.show()
+                time.sleep(50/1000.0)
