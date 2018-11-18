326c19cc8fec81673414ea63a9cb9acf6642e1e5
John Bell <jwbell001@gmail.com>
2018-11-18 20:01:33 +0200

    a

--- run.py
+++ run.py
@@ -1,6 +1,19 @@
-from saber import Saber
+import argparse
+from saber import Saber, rgb
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-c', '--clear', action='store_true', help='clear the display on exit')
+    args = parser.parse_args()
+
+    print('Press Ctrl-C to quit.')
+    if not args.clear:
+        print('Use "-c" argument to clear LEDs on exit')
+
     saber = Saber()
 
-    saber.run()
+    try:
+        saber.run()
+    except KeyboardInterrupt:
+        if args.clear:
+            saber.swipe(rgb(0, 0, 0), 200 / 1000.0)
